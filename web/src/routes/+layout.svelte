977ff08122adad72fb57b9940d7a78f251a45053
pch07 <philipch07@gmail.com>
2026-01-08 23:11:26 -0500

    Add + set xfm as favicon

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import './layout.css';
     import '98.css';
-    import favicon from '$lib/assets/favicon.svg';
+    import favicon from '$lib/assets/xfm.ico';
 
     let { children } = $props();
 </script>
