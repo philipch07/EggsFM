0fb9722216d64b2ed541391b8b393a00cadb774e
pch07 <philipch07@gmail.com>
2026-01-09 23:49:24 -0500

    Fix page.svelte lint

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -2,6 +2,6 @@
     import Player from '$lib/Player.svelte';
 </script>
 
-<div class="mx-auto w-full px-2 md:w-[500px] md:px-0">
+<div class="mx-auto w-full px-2 md:w-125 md:px-0">
     <Player />
 </div>
